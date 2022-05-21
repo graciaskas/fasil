80f327f63eb7c96daa12ec743eb1a67d2ac1ae13
Gracias Kasongo <makaveligracias@ao.com>
2022-05-21 09:20:31 +0200

    [Some changes]

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Motor Identification management system
 1.Identify motocycles
 2.Identify motor driver
+
+##### Requirements
+1. Node.js V12
+2.Npm v6.14.16
+3.MongoDB v4.4
---
+++ client/build/precache-manifest.fee3661073b34d867ac2ce136751b9eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196479237624a8f72357d57704ae28da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d07e76fa02298ebfbd4",
+    "url": "/static/css/2.455cd81f.chunk.css"
+  },
+  {
+    "revision": "084ee398579db5375489",
+    "url": "/static/css/main.224c298c.chunk.css"
+  },
+  {
+    "revision": "5d07e76fa02298ebfbd4",
+    "url": "/static/js/2.070ef219.chunk.js"
+  },
+  {
+    "revision": "3f67b7fb96f5fcf75cd1664e4ae297f5",
+    "url": "/static/js/2.070ef219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084ee398579db5375489",
+    "url": "/static/js/main.5c48237b.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
